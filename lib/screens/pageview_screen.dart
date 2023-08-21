6a6e7c838d0647a269134e0d4f186dba9a77231a
jaber <jaberqasem75@gmail.com>
2023-08-21 18:09:27 +0300

    home screen with animations

---
+++ lib/screens/pageview_screen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:prophets_stories/screens/home_screen.dart';
+
+class PageViewScreen extends StatefulWidget {
+  const PageViewScreen({super.key});
+
+  @override
+  State<PageViewScreen> createState() => _PageViewScreenState();
+}
+
+class _PageViewScreenState extends State<PageViewScreen> {
+  PageController _pageController = PageController();
+  int _currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ذكر و هدي',style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: (index) {
+                setState(() {
+                  _currentPageIndex = index;
+                });
+              },
+              children: <Widget>[
+                _buildPage('lib/assets/animation/ee.json', 'استمتع بلحظات الصفاء والتأمل في ظل القرآن والسنة النبوية'),
+                _buildPage('lib/assets/animation/rr.json', 'استرخِ وتأمل في رحمة الله وعظمته في عبادتك اليومية'),
+                _buildPage('lib/assets/animation/ww.json', 'استكشف جمال التواصل مع الله من خلال الصلوات والأذكار'),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                if (_currentPageIndex != 0)
+                  FloatingActionButton(
+                    onPressed: () {
+                      _pageController.nextPage(duration: Duration(milliseconds: 300), curve: Curves.easeInOut);
+                    },
+                    backgroundColor: Colors.teal,
+                    child: Icon(Icons.arrow_forward),
+                  ),
+                Row(
+                  children: List.generate(3, (index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(horizontal: 4),
+                      width: 10,
+                      height: 10,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: _currentPageIndex == index ? Colors.teal : Colors.grey,
+                      ),
+                    );
+                  }),
+                ),
+                if (_currentPageIndex != 2)
+                  FloatingActionButton(
+                    onPressed: () {
+                      _pageController.previousPage(duration: Duration(milliseconds: 300), curve: Curves.easeInOut);
+                    },
+                    backgroundColor: Colors.teal,
+                    child: Icon(Icons.arrow_back),
+                  ),
+                if (_currentPageIndex == 2)
+                  FloatingActionButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()),
+                      );
+                    },
+                    backgroundColor: Colors.teal,
+                    child: Icon(Icons.check),
+                  ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPage(String animationPath, String pageTitle) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Lottie.asset(animationPath, width: 350, height: 350),
+          SizedBox(height: 20),
+          Text(pageTitle, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold), textDirection: TextDirection.rtl),
+        ],
+      ),
+    );
+  }
+}
