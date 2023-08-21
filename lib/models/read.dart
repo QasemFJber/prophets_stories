c15878c9074cf79673705b82a92ee7dd3b5fc8c2
jaber <jaberqasem75@gmail.com>
2023-08-21 18:09:47 +0300

    home screen with animations

---
+++ lib/models/read.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+class Read{
+  String _title;
+  String _body;
+
+  Read(this._title, this._body);
+
+  String get body => _body;
+
+  set body(String value) {
+    _body = value;
+  }
+
+  String get title => _title;
+
+  set title(String value) {
+    _title = value;
+  }
+}
