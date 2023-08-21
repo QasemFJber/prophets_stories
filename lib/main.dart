c635af05310e69d1d6024eb09f6fc635a6faf010
jaber <jaberqasem75@gmail.com>
2023-08-21 18:09:06 +0300

    splash and login with register screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:prophets_stories/screens/conversations_screen.dart';
+import 'package:prophets_stories/screens/details_screen.dart';
 
 import 'package:prophets_stories/screens/login_screen.dart';
+import 'package:prophets_stories/screens/pageview_screen.dart';
 import 'package:prophets_stories/screens/register_screen.dart';
+import 'package:prophets_stories/screens/remembrance_screen.dart';
 import 'package:prophets_stories/screens/splash_screen.dart';
+import 'package:prophets_stories/screens/stories_screen.dart';
+import 'package:prophets_stories/screens/tutorials_screen.dart';
 
 
 void main () =>runApp(MyApp());
@@ -15,11 +21,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: '/splash_screen',
       routes: {
         '/splash_screen' : (context) =>SplashScreen(),
         '/login_screen' : (context) =>LoginScreen(),
         '/register_screen' : (context) =>RegisterScreen(),
+        '/page_view_screen' : (context) =>PageViewScreen(),
+        '/conversation_screen' : (context) =>ConversationScreen(),
+        '/remembrance_screen' : (context) =>RemembranceScreen(),
+        '/stories_screen' : (context) =>StoriesScreen(),
+        '/tutorials_screen' : (context) =>TutorialScreen(),
       },
     );
   }
