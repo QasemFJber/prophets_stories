a6a9c7f79ce643206da6b5191fe092a8f61de972
jaber <jaberqasem75@gmail.com>
2023-08-21 18:50:15 +0300

    home screen with animations

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,24 +1,56 @@
 import 'package:flutter/material.dart';
-class DetailsScreen extends StatefulWidget {
-  const DetailsScreen({super.key});
+import '../models/read.dart';
 
-  @override
-  State<DetailsScreen> createState() => _DetailsScreenState();
-}
+class DetailsScreen extends StatelessWidget {
+  final Read hadith;
 
-class _DetailsScreenState extends State<DetailsScreen> {
+  DetailsScreen(this.hadith);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.teal,
         title: Text(
-          'الشاشة الرئيسية',
+          'شاشة التفاصيل',
           style: TextStyle(color: Colors.white),
         ),
         centerTitle: true,
       ),
-
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // صورة جميلة (يمكن استبدال الصورة بصورة حقيقية)
+              Image.network('https://play-lh.googleusercontent.com/oEUl5tiOZpTrUqZrN6rIoMnHys22rZClkkctDTituCcQxTdIGn9EadfJo8y8mkwlUUo=w240-h480-rw', width: 150, height: 150),
+              SizedBox(height: 16),
+              // اسم الرواي للحديث
+              Text(
+                'اسم الرواي',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 8),
+              // نص الحديث
+              Text(
+                hadith.title,
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 16),
+              // محتوى الحديث كاملاً
+              Text(
+                hadith.body,
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
